b45c5251cecbb6449d66c2cc6c52a7ce5256aa64
Vladislav Vulchev <vvladoh@gmail.com>
2020-04-29 11:12:22 +0300

    Initial read input

--- CodersStrikeBack/coders_strike_back.cpp
+++ CodersStrikeBack/coders_strike_back.cpp
@@ -130,12 +130,40 @@ void Game::gameLoop() {
 //*************************************************************************************************************
 
 void Game::getGameInput() {
+	int laps;
+	cin >> laps; cin.ignore();
+	int checkpointCount;
+	cin >> checkpointCount; cin.ignore();
+	for (int i = 0; i < checkpointCount; i++) {
+		int checkpointX;
+		int checkpointY;
+		cin >> checkpointX >> checkpointY; cin.ignore();
+	}
 }
 
 //*************************************************************************************************************
 //*************************************************************************************************************
 
 void Game::getTurnInput() {
+	for (int i = 0; i < 2; i++) {
+		int x; // x position of your pod
+		int y; // y position of your pod
+		int vx; // x speed of your pod
+		int vy; // y speed of your pod
+		int angle; // angle of your pod
+		int nextCheckPointId; // next check point id of your pod
+		cin >> x >> y >> vx >> vy >> angle >> nextCheckPointId; cin.ignore();
+	}
+
+	for (int i = 0; i < 2; i++) {
+		int x2; // x position of the opponent's pod
+		int y2; // y position of the opponent's pod
+		int vx2; // x speed of the opponent's pod
+		int vy2; // y speed of the opponent's pod
+		int angle2; // angle of the opponent's pod
+		int nextCheckPointId2; // next check point id of the opponent's pod
+		cin >> x2 >> y2 >> vx2 >> vy2 >> angle2 >> nextCheckPointId2; cin.ignore();
+	}
 }
 
 //*************************************************************************************************************
@@ -148,6 +176,8 @@ void Game::turnBegin() {
 //*************************************************************************************************************
 
 void Game::makeTurn() {
+	cout << "8000 4500 100" << endl;
+	cout << "8000 4500 100" << endl;
 }
 
 //*************************************************************************************************************
